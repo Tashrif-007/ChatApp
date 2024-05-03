6f6515188b0312ca4155a258ccfe61961a06f97c
Tashrif2001 <tashrifpro@gmail.com>
2024-05-03 18:21:15 -0400

    fixed convo model and get messages

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,6 +1,7 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
 
+
 export const sendMessage = async (req, res) =>{
     try {
         const {message} = req.body;
@@ -33,6 +34,28 @@ export const sendMessage = async (req, res) =>{
 
     } catch (error) {
         console.log("Error in sendMessage controller: ",error.message);
+        res.status(500).json({error: "internal error"});
+    }
+}
+
+export const getMessage = async (req, res) => {
+    try {
+        const {id: userToChatId} = req.params;
+        const senderId = req.user._id;
+
+        const conversation = await Conversation.findOne({
+            participants: { $all: [senderId, userToChatId] },
+        }).populate("messages");
+
+        if(!conversation){
+            return res.status(200).json([]);
+        }
+
+        const message = conversation.messages;
+        res.status(200).json(conversation.messages);
+
+    } catch (error) {
+        console.log("Error in getMessage controller: ",error.message);
         res.status(500).json({error: "internal error"});
     }
 }
--- backend/models/conversation.model.js
+++ backend/models/conversation.model.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 
 const conversationSchema = new mongoose.Schema({
-    participants:{
-        type: [mongoose.Schema.Types.ObjectId],
+    participants:[{
+        type: mongoose.Schema.Types.ObjectId,
         ref: "User",
-    },
+    },],
     messages: [{
-        type: [mongoose.Schema.Types.ObjectId],
+        type: mongoose.Schema.Types.ObjectId,
         ref: "Message",
         default: []
     }
