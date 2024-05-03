463ae33d353579a31ca96158856c19e0a04533b4
Tashrif2001 <tashrifpro@gmail.com>
2024-05-03 14:57:25 -0400

    added messages endpoint, protectroute, message-convo schema

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,18 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.routes.js';
+import messageRoutes from './routes/message.routes.js';
 import connectToMongoDb from './db/connectToMongoDb.js';
-
+import cookieParser from 'cookie-parser';
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 // app.get("/", (req, res) => {
 //     res.send("Hello world");
 // });
