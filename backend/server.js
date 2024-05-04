a9ca8fafcf303ca3937f582b81582a26c0b21785
Tashrif2001 <tashrifpro@gmail.com>
2024-05-04 13:45:55 -0400

    backend done

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.routes.js';
+import userRoutes from './routes/user.routes.js';
 import messageRoutes from './routes/message.routes.js';
 import connectToMongoDb from './db/connectToMongoDb.js';
 import cookieParser from 'cookie-parser';
@@ -13,6 +14,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 // app.get("/", (req, res) => {
 //     res.send("Hello world");
 // });
